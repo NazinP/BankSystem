f166e61ddbcd9d28cdea09d521f43c3bcf5086b0
NazinP <nazinp@mail.ru>
2021-07-07 20:37:27 +0300

    Логика получения запроса из очереди

--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,15 +1,28 @@
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class FrontEnd {
-    private List<Request> requestList = new LinkedList<>();
+    private Queue<Request> requestList = new LinkedList<>();
 
     public synchronized void addRequest(Request request) throws InterruptedException {
-        while (requestList.size() == 2){
+        while (requestList.size() == 2) {
             wait();
         }
         requestList.add(request);
         System.out.println("Заявка " + request.toString() + " обрабатывается");
         notifyAll();
+    }
+
+    public synchronized Request getRequest() {
+        while (requestList.isEmpty()) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        Request request = requestList.poll();
+        return request;
     }
 }
